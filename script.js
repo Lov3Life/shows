56ddca50342e6d63b46057a40ed47fb561a058e0
Lov3Life <106180858+Lov3Life@users.noreply.github.com>
2022-09-21 21:09:14 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+
+window.onload = function(){
+    showsApp.init();
+}
+
+let showsApp ={
+    data: null,
+    searchInput: null,
+    showsDataSection: null,
+    init: function(){
+        console.log("app start")
+
+        this.searchInput = document.getElementById("search-input");
+        this.searchInput.addEventListener("keyup", (e)=>{
+            if(e.keyCode == 13){
+                console.log("enter")
+                this.loadData(this.searchInput.value);
+            }
+        });
+        this.showsDataSection = document.querySelector(".shows-data-section");
+        this.loadData("friends")
+    },
+    loadData: function(str){
+        fetch("https://api.tvmaze.com/search/shows?q=" + str.trim())
+        .then(response => response.json())
+        .then(data => this.dataReady(data))
+    },
+    dataReady: function(showData){
+        this.data = showData;;
+        let allBoxesHtml = "";
+        for(let i = 0; i < showData.length; i++){
+            let show = showData[i];
+            let score = show.score;
+            show = show.show;
+            let genres = show.genres.join(", ");
+            let imgSrc = null;
+            if(show.image){
+                imgSrc = show.image.medium;
+            } else{
+                imgSrc = "https://cdn.pixabay.com/photo/2013/07/12/17/47/test-pattern-152459__340.png"
+            }
+            let showTitle = null;
+            if(!show.name) continue;
+            showTitle = show.name;
+            let network = "-";
+            if(show.network) network = show.network.name;
+            let officialSite = "-";
+            if(show.officialSite) officialSite = show.officialSite;
+            let premiered = "-";
+            if(show.premiered) premiered = show.premiered;
+            let summary = show.summary;
+            summary = `
+                <p>Show: ${showTitle} </p>
+                <p>date: ${premiered} </p>
+                <p>Network: ${network} </p>
+                <br>
+            ` + summary;
+            allBoxesHtml += this.getShowBoxTemplate(imgSrc,showTitle,genres,summary);
+        }
+        this.showsDataSection.innerHTML = allBoxesHtml;
+    },
+    getShowBoxTemplate: function(imgSrc, title, genres, overview){
+        return `
+        <div class="show-box">
+            <img src="${imgSrc}" alt="">
+            <div class="show-title">${title}</div>
+            <div class="show-genres">${genres}</div>
+            <div class="show-overview">${overview}</div>
+        </div>
+        `;
+    }
+};
